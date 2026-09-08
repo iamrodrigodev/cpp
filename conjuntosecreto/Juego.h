06ba806edab66c23d289862b5a21daba1431ff00
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-09-08 07:53:14 -0500

    feat: agregar primer juego

---
+++ conjuntosecreto/Juego.h
@@ -0,0 +1,12 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include "Conjunto.h"
+
+const int CANTIDAD_SECRETOS = 5;
+
+void iniciarJuego();
+void generarConjuntoSecreto(Conjunto& conjunto, int cantidad);
+int pedirNumero(int min, int max);
+
+#endif
