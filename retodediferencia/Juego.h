e3799af819ecaebd186840316270aae5c0148b7e
rodrigodev.ts <rodrigodev.ts@gmail.com>
2026-09-08 08:11:00 -0500

    fix: mejorar juegos

--- retodediferencia/Juego.h
+++ retodediferencia/Juego.h
@@ -7,6 +7,7 @@ const int CANTIDAD_SECRETOS = 5;
 
 void iniciarJuego();
 void generarConjuntoSecreto(Conjunto& conjunto, int cantidad);
+bool validarRango(int numero, int min, int max);
 int pedirNumero(int min, int max);
 Conjunto pedirConjuntoPrueba();
 void evaluarConjuntoPrueba(const Conjunto& secreto, const Conjunto& prueba);
