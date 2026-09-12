10310c78dbd18e1efee7dd68e3dddb09f28e4236
Rodrigo Emerson Infanzon Acosta <rodrigodev.ts@gmail.com>
2026-09-12 17:02:17 -0500

    Organizar proyectos en carpetas e implementar juego del gusano (Snake)

---
+++ JuegoGusano/Juego.h
@@ -0,0 +1,12 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include "Gusano.h"
+#include "Tablero.h"
+
+void configurarConsola();
+void mostrarGameOver(int puntuacion, int manzanasComidas);
+void ejecutarPartida();
+void iniciarJuego();
+
+#endif
